109997d83e4b7d11973b8e67b02ef5d475ab0d90
Adriano Souza <148394428+Dev-Adrianoo@users.noreply.github.com>
2024-12-08 23:48:54 -0300

    projeto 70%

---
+++ script.js
@@ -0,0 +1,57 @@
+window.revelar = ScrollReveal({reset:true}) 
+
+//TOPO SITE
+revelar.reveal('.efeito-texto-topo',{
+    duration: 2000,
+    distance: '90px'
+});
+
+revelar.reveal('.efeito-imagem-topo',{
+    duration: 2000,
+    distance: '90px',
+    delay: 700
+});
+
+revelar.reveal('.efeito-card1',{
+    duration: 5000,
+    distance: '130px',
+    delay: 500
+});
+
+revelar.reveal('.efeito-card2',{
+    duration: 5000,
+    distance: '130px',
+    delay: 1000
+});
+
+revelar.reveal('.efeito-card3',{
+    duration: 5000,
+    distance: '130px',
+    delay: 1500
+});
+
+revelar.reveal('.efeito-card4',{
+    duration: 5000,
+    distance: '130px',
+    delay: 2000
+});
+
+revelar.reveal('.efeito-card5',{
+    duration: 5000,
+    distance: '130px',
+    delay: 2000
+});
+
+revelar.reveal('.efeito-texto-sobre',{
+    duration: 5000,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.efeito-imagem-sobre',{
+    duration: 5000,
+    distance: '90px',
+    delay: 1000,
+    origin: 'right'
+});
